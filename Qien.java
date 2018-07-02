439e28a7857627d77ec4ec765539e5786300d052
Felixvl <digitaleservice@gmail.com>
2018-07-02 11:19:23 +0200

    eerste commit

---
+++ Qien.java
@@ -0,0 +1,17 @@
+class Qien{
+	public static void main(String[] args){
+		System.out.println("de tekst die je wilt typen");
+		int a = 4;
+		a = 6;
+		int b;
+		b = 7;
+		System.out.println(b);
+		if(a == b){
+			System.out.println("DEZE: a is kleiner dan b");
+		}else{
+			System.out.println("b is kleiner dan a");
+		}
+
+	}
+
+}
